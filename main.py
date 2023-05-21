445111b8cf8426018de7ab4b05fa868546853bd5
Aleksandra Biga <134082411+bisiiia@users.noreply.github.com>
2023-05-21 22:53:37 +0200

    plansza

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import os
+from colorama import Fore, Back
 
+def printWhite(data,ends):
+    print(Fore.WHITE,data,end=ends,sep="")
+
+tab = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+def draw(argument):
+    os.system('clear')
+    printWhite("┌───┬───┬───┬───┬───┬───┬───┐","\n")
+    for j in range(0, 6):
+        printWhite("│ ","")
+        for i in range(0,7):
+            if argument[i]!=0:
+                print(argument[i],end=" │ ")
+            else:
+                printWhite(" "," │ ")
+        if j!=5:
+            printWhite("\n├───┼───┼───┼───┼───┼───┼───┤","\n")
+        else:
+            printWhite("\n└───┴───┴───┴───┴───┴───┴───┘","\n")
+
+draw(tab)
